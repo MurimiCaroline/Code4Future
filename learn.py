f2cbd6e7b505d62198a2e4d8f974990917af825d
MurimiCaroline <murimicaroline20@gmail.com>
2023-11-12 12:38:36 +0300

    add break statement

---
+++ learn.py
@@ -0,0 +1,17 @@
+def multiply_two_numbers():
+  # Prompt the user for the first number.
+  num1 = float(input("Enter the first number: "))
+
+  # Prompt the user for the second number.
+  num2 = float(input("Enter the second number: "))
+
+  # Calculate the product of the two numbers.
+  product = num1 * num2
+
+  # Return the product.
+  return product
+
+# Print the product.
+print(multiply_two_numbers())
+
+
